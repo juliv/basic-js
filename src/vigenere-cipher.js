3a750c19833946be374dac6f0fb3d0d5c28c3201
Ivan Gavrilov <julivgan@gmail.com>
2022-05-03 01:03:02 +0400

    feat: task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,21 @@
-const { NotImplementedError } = require('../extensions/index.js');
+
+function repeatString(first, second) {
+  let result = ''
+  let firstLen = first.length
+  let it = 0
+  for (let i = 0; i < second.length; i++) {
+    if (i % firstLen === 0) {
+      it = 0
+    }
+    result += first[it]
+    it++
+  }
+  return result
+}
+
+function reverseString(str) {
+  return str.split('').reverse().join('')
+}
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,14 +37,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = Boolean(direct)
+    this.alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.alphaLen = this.alpha.length
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!')
+    }
+    let result = ''
+    const msg = message.toUpperCase()
+    const k = repeatString(key, message).toUpperCase()
+    let kIdx = 0
+
+    for (let it = 0; it < msg.length; it++) {
+      const i = this.alpha.indexOf(msg[it])
+      if (i === -1) {
+        result += msg[it]
+      }
+      else {
+        const j = this.alpha.indexOf(k[kIdx])
+        const val = j + i
+        let letIdx = 0
+        if (val > 0) {
+          letIdx = val % this.alphaLen
+        }
+        result += this.alpha[letIdx]
+        kIdx++
+      }
+    }
+    return this.direct ? result : reverseString(result)
   }
+
+  decrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let result = ''
+    const msg = message.toUpperCase()
+    const k = repeatString(key, message).toUpperCase()
+    let kIdx = 0
+
+    for (let it = 0; it < msg.length; it++) {
+      const i = this.alpha.indexOf(msg[it])
+      if (i === -1) {
+        result += msg[it]
+      }
+      else {
+        const j = this.alpha.indexOf(k[kIdx])
+        const val = (i - j) % this.alphaLen
+        let letIdx = 0
+        letIdx = val > 0 ? val : (val + this.alphaLen) % this.alphaLen
+        result += this.alpha[letIdx]
+        kIdx++
+      }
+    }
+    return this.direct ? result : reverseString(result)
+  }
+
 }
 
 module.exports = {
